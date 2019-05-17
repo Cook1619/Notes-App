4269a716e9adedbaffa495d8f0c4bb5f68f5be60
Matt <cook6348@gmail.com>
2019-05-16 19:18:49 -0500

    Fixed bracket error

--- notes.js
+++ notes.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
+const fs = require('fs')
 const chalk = require('chalk')
 
 const getNotes = function () {
-    return 'Your notes....'
+    return 'Your notes...'
 }
 
 const addNote = function (title, body) {
@@ -17,11 +17,24 @@ const addNote = function (title, body) {
             body: body
         })
         saveNotes(notes)
-        console.log(chalk.green.inverse('Note Added!')
+        console.log(chalk.green.inverse('New note added!'))
     } else {
-        console.log(chalk.red.inverse('Note title taken!!')
+        console.log(chalk.red.inverse('Note title taken!'))
     }
+}
 
+const removeNote = function (title) {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(function (note) {
+        return note.title !== title
+    })
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No note found!'))
+    }    
 }
 
 const saveNotes = function (notes) {
@@ -31,29 +44,16 @@ const saveNotes = function (notes) {
 
 const loadNotes = function () {
     try {
-        const dataBuffer = fs.readFileSync('notes.json');
-        const dataJSON = dataBuffer.toString();
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
         return JSON.parse(dataJSON)
     } catch (e) {
-        return [];
-    }
-}
-
-const removedNote = function (title) {
-    const notes = loadNotes();
-    const notesToKeep = notes.filter(function(note){
-        return note.title !== title
-    })
-    if (notes.length > notesToKeep.length){
-        console.log(chalk.green.inverse('Note Removed!!'))
-        saveNotes(notesToKeep)
-    }else {
-        console.log(chalk.red.inverse('No Note found!'))
+        return []
     }
 }
 
 module.exports = {
-    getNotes,
-    addNote,
-    removedNote
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
 }
